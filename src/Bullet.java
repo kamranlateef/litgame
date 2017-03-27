abf692a59ea9e5c0f0ee397c04f78664795cb6de
kamranlateef <kamran_lateef@student.wayland.k12.ma.us>
2017-03-27 10:35:35 -0400

    added a bullet class fixed the rocketship speed made the rocketship be able to return off screen to other side.

---
+++ src/Bullet.java
@@ -0,0 +1,15 @@
+/**
+ * Created by kamran_lateef on 3/27/17.
+ */
+public class Bullet extends Sprite{
+    public Bullet(int x, int y){
+        this.x = x;
+        this.y =y;
+    }
+    @Override
+    public void update(){
+        super.update();
+    }
+
+
+}
